0bb9661bf295a96d6780ec426b6d78d8fb4a2680
chisoftltd <chisoftltd@gmail.com>
2017-07-27 22:18:09 +0100

    further commit

--- index.php
+++ index.php
@@ -5,25 +5,6 @@
  * Date: 27/07/2017
  * Time: 20:04
  */
-ob_start();
-session_start();
-try{
-    require_once 'db/dbconnect.php';
-}
-catch (Exception $e){
-    echo $e->getMessage();
-}
-
-$sql = "SELECT * FROM menu ORDER BY id";
-$stmt = $link->prepare($sql);
-$stmt->execute();
-
-if($stmt->rowCount()){
-    die("Found");
-}
-
-// Report all errors
-error_reporting(E_ALL);
 
 ?>
 <head>

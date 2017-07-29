50d09eb50080338ef0e5c76c930c28a8f01d6360
chisoftltd <chisoftltd@gmail.com>
2017-07-29 14:14:22 +0100

    further commit

--- include/signinheader.php
+++ include/signinheader.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+Header information when user is signed on or logged in, with company logo
 -->
 <header>
     <div id="logo">
-        <a href="index.php"><img src="images/WebOilLogo.png" alt="Company logo" /></a>
+        <a href="signinindex.php" style="top: 10px"><img src="images/WebOilLogo.png" alt="Company logo"/></a>
     </div>
-    <form style="float: right; position:absolute; right: 5%; top: 10%">
-        Search:
-        <input type="search" name="googlesearch">
-        <input type="Submit">
-    </form>
     <nav>
         <ul class="header-links">
             <li><a href="index.php">Home</a></li>

2d82fc636e3c378977c6ac30c55effa2d930c1d1
chisoftltd <chisoftltd@gmail.com>
2017-07-29 12:57:11 +0100

    further commit

--- index.php
+++ index.php
@@ -2,76 +2,101 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Web Application Description - RGUEthics!</title>
+    <title>Application Description - WebOil!</title>
     <link rel="stylesheet" href="css/main-style.css">
     <link rel="stylesheet" href="css/welllog-style.css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <!-- jPList core -->
     <link href="style/jplist.core.min.css" rel="stylesheet" type="text/css"/>
     <script src="js/jplist.core.min.js"></script>
 </head>
 <body>
-<div>
+<header>
     <?php include 'include/header.php'; ?>
-</div>
-<div>
-    <hr>
-</div>
-<div class="pageContent">
-    <nav class="nav">
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <br>
-            <li><a href="about.php">About Us</a></li>
-            <br>
-            <li><a href="contact.php">Contact</a></li>
-            <br>
-            <li><a href="login.php">Login</a></li>
-            <br>
-        </ul>
-    </nav>
-    <article class="article">
-        <h2>Research Ethics and Integrity System</h2>
-        <p>A Full Research Ethics and Integrity Assessment is required before, during and maybe after a research
-            project. Most research institution and centers are
-            commitment to promote and facilitate the conduct of research ethics and integrity.</p>
-        <h3>Purpose of Ethical Standards</h3>
-        <p>In line with acceptable police and framework, RGU attaches great importance to addressing the ethical
-            implications of all research activities
-            carried out by its members, be they undergraduates, postgraduates or academic members of staff.
-            Attention to the ethical and legal implications of research for researchers, research subjects, sponsors and
-            collaborators is an intrinsic
-            part of good research <a
-                    href="http://www.ed.ac.uk/geosciences/intranet/working-in-school/other-important-information/ethicsinresearch">practice.</a>
-        </p>
-        <p>You need to assess whether your project needs an ethical submission. This can be done by completing the RESSA
-            form and based on the outcome decide whether an application is needed.</p>
-        <p><a class="more"
-              href="http://www.rgu.ac.uk/download.cfm?downloadfile=5E84DCA0-2BEB-11E1-8D06000D609CAA9F&typename=dmFile&fieldname=filename">Student
-                and Supervisor Appraisal (RESSA) Form</a></p>
-        <h3>Assessment of Research Ethics is very important expecially when the following groups are involved:</h3>
-        <ul>
-            <li>vulnerable human subjects (e.g. children, people with cognitive disabilities and so on)
-            </li>
-            <br>
-            <li>invasive procedures or addressing sensitive issues (e.g. video-taping without informed consent,
-                questions about sexuality or about criminal<br> behaviour)
-            </li>
-            <br>
-            <li>biophysical research which requires extraordinary permission from landowners, involves significant
-                disturbance to vulnerable species or habitats,<br> sampling rare/endangered or harmful taxa/species,
-                and/or transporting samples/specimens between countries or significant ‘boundaries’.
-            </li>
-            <br>
-        </ul>
+</header>
+<section>
+    <div class="pageContent">
+        <nav class="nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <br>
+                <li><a href="about.php">About Us</a></li>
+                <br>
+                <li><a href="contact.php">Contact</a></li>
+                <br>
+                <li><a href="login.php">Login</a></li>
+                <br>
+            </ul>
+        </nav>
+    </div>
+    <div id="all-topic">
 
-    </article>
-</div>
-<div>
-    <hr>
-</div>
-<div>
-    <?php include 'include/footer.php'; ?>
-</div>
+        <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
+        <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
+        <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
+        <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
+        <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
+        <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
+        <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
+        <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
+        <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
+        <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
+        <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
+        <a href="http://www.google.com"> <div id="topic">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div></a>
+
+    </div>
+
+    <div>
+        <hr>
+    </div>
+    <div >
+
+        <article class="article">
+            <h2>Research Ethics and Integrity System</h2>
+            <p>A Full Research Ethics and Integrity Assessment is required before, during and maybe after a research
+                project. Most research institution and centers are
+                commitment to promote and facilitate the conduct of research ethics and integrity.</p>
+            <h3>Purpose of Ethical Standards</h3>
+            <p>In line with acceptable police and framework, RGU attaches great importance to addressing the ethical
+                implications of all research activities
+                carried out by its members, be they undergraduates, postgraduates or academic members of staff.
+                Attention to the ethical and legal implications of research for researchers, research subjects, sponsors and
+                collaborators is an intrinsic
+                part of good research <a
+                        href="http://www.ed.ac.uk/geosciences/intranet/working-in-school/other-important-information/ethicsinresearch">practice.</a>
+            </p>
+            <p>You need to assess whether your project needs an ethical submission. This can be done by completing the RESSA
+                form and based on the outcome decide whether an application is needed.</p>
+            <p><a class="more"
+                  href="http://www.rgu.ac.uk/download.cfm?downloadfile=5E84DCA0-2BEB-11E1-8D06000D609CAA9F&typename=dmFile&fieldname=filename">Student
+                    and Supervisor Appraisal (RESSA) Form</a></p>
+            <h3>Assessment of Research Ethics is very important expecially when the following groups are involved:</h3>
+            <ul>
+                <li>vulnerable human subjects (e.g. children, people with cognitive disabilities and so on)
+                </li>
+                <br>
+                <li>invasive procedures or addressing sensitive issues (e.g. video-taping without informed consent,
+                    questions about sexuality or about criminal<br> behaviour)
+                </li>
+                <br>
+                <li>biophysical research which requires extraordinary permission from landowners, involves significant
+                    disturbance to vulnerable species or habitats,<br> sampling rare/endangered or harmful taxa/species,
+                    and/or transporting samples/specimens between countries or significant ‘boundaries’.
+                </li>
+                <br>
+            </ul>
+
+        </article>
+    </div>
+    <div>
+        <hr>
+    </div>
+</section>
+<footer>
+    <div>
+        <?php include 'include/footer.php'; ?>
+    </div>
+</footer>
 </body>
 </html>

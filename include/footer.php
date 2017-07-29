aa64525c5239e5e489bfc1a34055a6099d4edf99
chisoftltd <chisoftltd@gmail.com>
2017-07-29 14:41:38 +0100

    further commit

---
+++ include/footer.php
@@ -0,0 +1,26 @@
+<!-- footer file - For lack of time link here are directed to certain RGU pages-->
+<footer>
+    <nav>
+        <form>
+            <hr>
+        </form>
+        <p style="text-align: center;">
+            <strong>Follow us</strong>
+        </p>
+        <ul class="footer-links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="http://www.rgu.ac.uk/footer/disclaimer/disclaimer/">Disclaimer</a></li>
+            <li>
+                <a href="http://www.rgu.ac.uk/about/governance/information-governance/freedom-of-information/freedom-of-information/">Freedom
+                    of information</a></li>
+            <li><a href="http://www.rgu.ac.uk/footer/privacy-and-cookie-statement/privacy-and-cookie-statement/">Privacy
+                    and cookie statement</a></li>
+            <li><a href="http://www.rgu.ac.uk/footer/complaints/complaints/">Complaints</a></li>
+            <!--<li><a href="http://www.rgu.ac.uk/footer/copyright-2017/copyright/">Copyright ©2017</a></li>-->
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <p style="text-align: center;">
+            &copy; 2016-<?php echo date("Y"); ?>
+        </p>
+    </nav>
+</footer>
--- index.php
+++ index.php
@@ -111,48 +111,18 @@ include_once 'db/dbconnect.php';
 
         <article class="article">
             <h2>WebOil Elearning Soultion</h2>
-            <p>A Full Research Ethics and Integrity Assessment is required before, during and maybe after a research
-                project. Most research institution and centers are
-                commitment to promote and facilitate the conduct of research ethics and integrity.</p>
-            <h3>Purpose of Ethical Standards</h3>
-            <p>In line with acceptable police and framework, RGU attaches great importance to addressing the ethical
-                implications of all research activities
-                carried out by its members, be they undergraduates, postgraduates or academic members of staff.
-                Attention to the ethical and legal implications of research for researchers, research subjects, sponsors
-                and
-                collaborators is an intrinsic
-                part of good research <a
-                        href="http://www.ed.ac.uk/geosciences/intranet/working-in-school/other-important-information/ethicsinresearch">practice.</a>
-            </p>
-            <p>You need to assess whether your project needs an ethical submission. This can be done by completing the
-                RESSA
-                form and based on the outcome decide whether an application is needed.</p>
-            <p><a class="more"
-                  href="http://www.rgu.ac.uk/download.cfm?downloadfile=5E84DCA0-2BEB-11E1-8D06000D609CAA9F&typename=dmFile&fieldname=filename">Student
-                    and Supervisor Appraisal (RESSA) Form</a></p>
-            <h3>Assessment of Research Ethics is very important expecially when the following groups are involved:</h3>
-            <ul>
-                <li>vulnerable human subjects (e.g. children, people with cognitive disabilities and so on)
-                </li>
-                <br>
-                <li>invasive procedures or addressing sensitive issues (e.g. video-taping without informed consent,
-                    questions about sexuality or about criminal<br> behaviour)
-                </li>
-                <br>
-                <li>biophysical research which requires extraordinary permission from landowners, involves significant
-                    disturbance to vulnerable species or habitats,<br> sampling rare/endangered or harmful taxa/species,
-                    and/or transporting samples/specimens between countries or significant ‘boundaries’.
-                </li>
-                <br>
-            </ul>
 
         </article>
     </div>
-    <div>
-        <hr>
-    </div>
-</section>
+    <form>
+        <hr> <!-- draw a line-->
+    </form>
+</section><!-- end of section-->
+<form>
+    <hr> <!-- draw a line-->
+</form>
 <footer>
+    <!-- footer area-->
     <div>
         <?php include 'include/footer.php'; ?>
     </div>

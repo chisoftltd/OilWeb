d055c13db9cb104cce61fc5ddb13ee226deb368b
chisoftltd <chisoftltd@gmail.com>
2017-07-31 19:35:01 +0100

    further commit

--- courses/welldrilling.php
+++ courses/welldrilling.php
@@ -19,59 +19,84 @@ include_once '/db/dbconnect.php';
 <html>
 <head>
     <meta charset="utf-8">
-    <title>WebOil Topic - Well Drilling!</title>
+    <title>Application Description - WebOil!</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <!-- Add css file-->
     <!-- <link href="css/styles.css" rel="stylesheet" type="text/css"/>-->
-    <link rel="stylesheet" href="/css/main-style.css">
+    <link rel="stylesheet" href="css/main-style.css">
 </head>
 <body><!-- Body area start-->
 
 <!-- add top navigational bar using bootstrap-->
-<nav class="navbar navbar-default" role="navigation">
+<nav class="navbar navbar-inverse" role="navigation">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
-                <span class="sr-only">Toggle navigation</span>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navweboil">
+                <!--<span class="sr-only">Toggle navigation</span>-->
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="index.php">Home | WebOil Elearning Solution</a>
         </div>
-        <div class="collapse navbar-collapse" id="navbar1">
+        <div class="collapse navbar-collapse" id="navweboil">
             <ul class="nav navbar-nav navbar-right">
                 <!-- check if same user is still same as the active session user and load appropriate menu options -->
                 <?php if (isset($_SESSION['usr_id'])) { ?>
                     <li class="active"><a href="signinindex.php">Home</a></>
-                    <li><a href="/menu/about.php">About Us</a></li>
-                    <li><a href="/menu/courses.php">Courses</a></li>
-                    <li><a href="/menu/assessment.php">Assessment</a></li>
-                    <li><a href="/menu/submission.php">Submission</a></li>
-                    <li><a href="/menu/demo.php">Demo</a></li>
-                    <li><a href="/menu/contact.php">Contact Us</a></li>
-                    <li><a href="/menu/help.php">Help</a></li>
-                    <li><p class="navbar-text">Signed in as <?php echo $_SESSION['usr_name']; ?></p></li>
-                    <li><a href="/index.php">Log Out</a></li>
+                    <li><a href="menu/about.php">About Us</a></li>
+                    <li><a href="menu/courses.php">Courses</a></li>
+                    <li><a href="menu/assessment.php">Assessment</a></li>
+                    <li><a href="menu/submission.php">Submission</a></li>
+                    <li><a href="menu/demo.php">Demo</a></li>
+                    <li><a href="menu/contact.php">Contact Us</a></li>
+                    <li><a href="menu/help.php">Help</a></li>
+                    <li><p class="navbar-text"><span
+                                    class="glyphicon glyphicon-user">Signed in as <?php echo $_SESSION['usr_name']; ?>
+                        </p></li>
+                    <li><a href="/index.php"><span class="glyphicon glyphicon-log-out">Log Out</a></li>
+                    <form class="navbar-form navbar-right">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit">
+                                    <i class="glyphicon glyphicon-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 <?php } else { ?>
-                    <!--<li class="active"><a href="signinindex.php">Home</a></>-->
-                    <li class="active"><a href="/index.php">Home</a></>
-                    <li><a href="/menu/about.php">About Us</a></li>
-                    <li><a href="/menu/courses.php">Courses</a></li>
-                    <li><a href="/menu/assessment.php">Assessment</a></li>
-                    <li><a href="/menu/submission.php">Submission</a></li>
-                    <li><a href="/menu/demo.php">Demo</a></li>
-                    <li><a href="/menu/contact.php">Contact Us</a></li>
-                    <li><a href="/menu/help.php">Help</a></li>
-                    <li><a href="/menu/login.php">Login</a></li>
-                    <li><a href="/menu/register.php">Register</a></li>
+                    <li class="active"><a href="index.php">Home</a></>
+                    <li><a href="menu/about.php">About Us</a></li>
+                    <li><a href="menu/courses.php">Courses</a></li>
+                    <li><a href="menu/assessment.php">Assessment</a></li>
+                    <li><a href="menu/submission.php">Submission</a></li>
+                    <li><a href="menu/demo.php">Demo</a></li>
+                    <li><a href="menu/contact.php">Contact Us</a></li>
+                    <li><a href="menu/help.php">Help</a></li>
+                    <li><a href="menu/login.php"><span class="glyphicon glyphicon-log-in">Login</a></li>
+                    <li><a href="menu/register.php"><span class="glyphicon glyphicon-user"></span>Register</a></li>
+                    <form class="navbar-form navbar-right">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit">
+                                    <i class="glyphicon glyphicon-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 <?php } ?>
             </ul>
         </div>
     </div>
 </nav>
+
 
 <header>
     <?php if (isset($_SESSION['usr_id'])) { ?>

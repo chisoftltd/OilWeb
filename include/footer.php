1bdb7fa4f33196057a2f76ffb82654d44b5bb934
chisoftltd <chisoftltd@gmail.com>
2017-07-31 21:53:17 +0100

    further commit

--- include/footer.php
+++ include/footer.php
@@ -9,17 +9,17 @@
         </p>
         <ul class="footer-links">
 
-                <li><a href="/index.php">Home</a></li>
-                <div class="row">
-                    <div class="col-sm-12 social">
-                        <a href="http://www.facebook.com/pages/Azmindcom/196582707093191"><i class="fa fa-facebook"></i></a>
-                        <span class="divider-2"></span>
-                        <a href="http://twitter.com/anli_zaimi"><i class="fa fa-twitter"></i></a>
-                        <span class="divider-2"></span>
-                        <a href="https://plus.google.com/101131425868807087570"><i class="fa fa-google-plus"></i></a>
-                    </div>
+            <li><a href="/index.php">Home</a></li>
+            <div class="row">
+                <div class="col-sm-12 social">
+                    <a href="http://www.facebook.com/pages/Azmindcom/196582707093191"><i class="fa fa-facebook"></i></a>
+                    <span class="divider-2"></span>
+                    <a href="http://twitter.com/anli_zaimi"><i class="fa fa-twitter"></i></a>
+                    <span class="divider-2"></span>
+                    <a href="https://plus.google.com/101131425868807087570"><i class="fa fa-google-plus"></i></a>
                 </div>
-                <li><a href="/contact.php">Contact</a></li>
+            </div>
+            <li><a href="/contact.php">Contact</a></li>
 
         </ul>
 

c02bd8592623c23b490cf59669f4a5c8e4197f25
chisoftltd <chisoftltd@gmail.com>
2017-07-31 22:03:15 +0100

    further commit

--- courses/wellcontrol.php
+++ courses/wellcontrol.php
@@ -22,6 +22,7 @@ include_once '../db/dbconnect.php';
     <title>WebOil - Drilling</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>

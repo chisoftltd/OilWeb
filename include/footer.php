35c281bd26b3c7d95925085d1d6c58882eaddbbc
chisoftltd <chisoftltd@gmail.com>
2017-07-31 21:32:27 +0100

    further commit

--- include/footer.php
+++ include/footer.php
@@ -9,14 +9,15 @@
         </p>
         <ul class="footer-links">
             <li><a href="index.php">Home</a></li>
-            <li><a href="http://www.rgu.ac.uk/footer/disclaimer/disclaimer/">Disclaimer</a></li>
-            <li>
-                <a href="http://www.rgu.ac.uk/about/governance/information-governance/freedom-of-information/freedom-of-information/">Freedom
-                    of information</a></li>
-            <li><a href="http://www.rgu.ac.uk/footer/privacy-and-cookie-statement/privacy-and-cookie-statement/">Privacy
-                    and cookie statement</a></li>
-            <li><a href="http://www.rgu.ac.uk/footer/complaints/complaints/">Complaints</a></li>
-            <!--<li><a href="http://www.rgu.ac.uk/footer/copyright-2017/copyright/">Copyright ©2017</a></li>-->
+            <div class="row">
+                <div class="col-sm-12 social">
+                    <a href="http://www.facebook.com/pages/Azmindcom/196582707093191"><i class="fa fa-facebook"></i></a>
+                    <span class="divider-2"></span>
+                    <a href="http://twitter.com/anli_zaimi"><i class="fa fa-twitter"></i></a>
+                    <span class="divider-2"></span>
+                    <a href="https://plus.google.com/101131425868807087570"><i class="fa fa-google-plus"></i></a>
+                </div>
+            </div>
             <li><a href="contact.php">Contact</a></li>
         </ul>
         <p style="text-align: center;">

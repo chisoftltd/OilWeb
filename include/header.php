92b3c3053fb1201fb63f260401c332cec6d95a6a
chisoftltd <chisoftltd@gmail.com>
2017-07-31 18:20:34 +0100

    further commit

--- include/header.php
+++ include/header.php
@@ -8,11 +8,11 @@ and open the template in the editor.
     <div id="logo">
         <a href="/index.php"><img src="/images/WebOilLogo.png" alt="Company logo" /></a>
     </div>
-    <form style="float: right; position:absolute; right: 5%; top: 10%">
+   <!-- <form style="float: right; position:absolute; right: 5%; top: 10%">
         Search:
         <input type="search" name="googlesearch">
         <input type="Submit">
-    </form>
+    </form>-->
     <nav>
         <ul class="header-links">
             <li><a href="/index.php">Home</a></li>

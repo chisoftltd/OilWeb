253753ab908cce59585e79b76944f857feda428e
chisoftltd <chisoftltd@gmail.com>
2017-07-31 21:52:56 +0100

    further commit

--- include/footer.php
+++ include/footer.php
@@ -8,19 +8,19 @@
             <strong>Follow us</strong>
         </p>
         <ul class="footer-links">
-            <div style="width : 200px">
-            <li><a href="/index.php">Home</a></li>
-            <div class="row">
-                <div class="col-sm-12 social">
-                    <a href="http://www.facebook.com/pages/Azmindcom/196582707093191"><i class="fa fa-facebook"></i></a>
-                    <span class="divider-2"></span>
-                    <a href="http://twitter.com/anli_zaimi"><i class="fa fa-twitter"></i></a>
-                    <span class="divider-2"></span>
-                    <a href="https://plus.google.com/101131425868807087570"><i class="fa fa-google-plus"></i></a>
+
+                <li><a href="/index.php">Home</a></li>
+                <div class="row">
+                    <div class="col-sm-12 social">
+                        <a href="http://www.facebook.com/pages/Azmindcom/196582707093191"><i class="fa fa-facebook"></i></a>
+                        <span class="divider-2"></span>
+                        <a href="http://twitter.com/anli_zaimi"><i class="fa fa-twitter"></i></a>
+                        <span class="divider-2"></span>
+                        <a href="https://plus.google.com/101131425868807087570"><i class="fa fa-google-plus"></i></a>
+                    </div>
                 </div>
-            </div>
-            <li><a href="/contact.php">Contact</a></li>
-            </div>
+                <li><a href="/contact.php">Contact</a></li>
+
         </ul>
 
         <p style="text-align: center;">

b96b93a07247bb4b6e3b7427de5d55678ea70fb1
chisoftltd <chisoftltd@gmail.com>
2017-08-02 18:07:09 +0100

    further commit

--- menu/submission.php
+++ menu/submission.php
@@ -112,7 +112,7 @@ ini_set('display_errors', 1);
     <div class="navbar navbar-default navbar-static-top">
         <div class="container">
             <div class="navbar-header">
-                <a class="navbar-brand" href="index.php">PHP File Uploader</a>
+                <a class="navbar-brand" href="/index.php">PHP File Uploader</a>
             </div>
         </div>
     </div>
@@ -124,10 +124,15 @@ ini_set('display_errors', 1);
             <div class="col-lg-12">
                 <form class="well" action="submission.php" method="post" enctype="multipart/form-data">
                     <div class="form-group">
-                        <label for="file">Select a file to upload</label>
-                        <input type="file" name="file">
-                        <p class="help-block">Only jpg,jpeg,png and gif file with maximum size of 1 MB is allowed.</p>
-                    </div>
+                        <table width="350" border="0" cellpadding="1" cellspacing="1" class="box">
+                            <tr>
+                                <td width="246">
+                                    <input type="hidden" name="MAX_FILE_SIZE" value="2000000">
+                                    <input name="userfile" type="file" id="userfile">
+                                </td>
+                                <td width="80"><input name="upload" type="submit" class="box" id="upload" value=" Upload "></td>
+                            </tr>
+                        </table></div>
                     <input type="submit" class="btn btn-lg btn-primary" value="Upload">
                 </form>
             </div>
@@ -135,62 +140,30 @@ ini_set('display_errors', 1);
     </div> <!-- /container -->
 
     <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        $name     = $_FILES['file']['name'];
-        $tmpName  = $_FILES['file']['tmp_name'];
-        $error    = $_FILES['file']['error'];
-        $size     = $_FILES['file']['size'];
-        $ext      = strtolower(pathinfo($name, PATHINFO_EXTENSION));
+    if(isset($_POST['upload']) && $_FILES['userfile']['size'] > 0)
+    {
+        $fileName = $_FILES['userfile']['name'];
+        $tmpName  = $_FILES['userfile']['tmp_name'];
+        $fileSize = $_FILES['userfile']['size'];
+        $fileType = $_FILES['userfile']['type'];
 
-        switch ($error) {
-            case UPLOAD_ERR_OK:
-                $valid = true;
-                //validate file extensions
-                if ( !in_array($ext, array('jpg','jpeg','png','gif')) ) {
-                    $valid = false;
-                    $response = 'Invalid file extension.';
-                }
-                //validate file size
-                if ( $size/1024/1024 > 2 ) {
-                    $valid = false;
-                    $response = 'File size is exceeding maximum allowed size.';
-                }
-                //upload file
-                if ($valid) {
-                    $targetPath =  dirname( __FILE__ ) . DIRECTORY_SEPARATOR. 'uploads' . DIRECTORY_SEPARATOR. $name;
-                    move_uploaded_file($tmpName,$targetPath);
-                    header( 'Location: index.php' ) ;
-                    exit;
-                }
-                break;
-            case UPLOAD_ERR_INI_SIZE:
-                $response = 'The uploaded file exceeds the upload_max_filesize directive in php.ini.';
-                break;
-            case UPLOAD_ERR_FORM_SIZE:
-                $response = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.';
-                break;
-            case UPLOAD_ERR_PARTIAL:
-                $response = 'The uploaded file was only partially uploaded.';
-                break;
-            case UPLOAD_ERR_NO_FILE:
-                $response = 'No file was uploaded.';
-                break;
-            case UPLOAD_ERR_NO_TMP_DIR:
-                $response = 'Missing a temporary folder. Introduced in PHP 4.3.10 and PHP 5.0.3.';
-                break;
-            case UPLOAD_ERR_CANT_WRITE:
-                $response = 'Failed to write file to disk. Introduced in PHP 5.1.0.';
-                break;
-            case UPLOAD_ERR_EXTENSION:
-                $response = 'File upload stopped by extension. Introduced in PHP 5.2.0.';
-                break;
-            default:
-                $response = 'Unknown error';
-                break;
+        $fp      = fopen($tmpName, 'r');
+        $content = fread($fp, filesize($tmpName));
+        $content = addslashes($content);
+        fclose($fp);
+
+        if(!get_magic_quotes_gpc())
+        {
+            $fileName = addslashes($fileName);
         }
 
-        echo $response;
+        $query = "INSERT INTO upload (fileName, fileSize, fileType, fileContent ) ".
+            "VALUES ('$fileName', '$fileSize', '$fileType', '$content')";
+
+        mysqli_query($link, $query) or die('Error, query failed');
+
+        echo "<br>File $fileName uploaded<br>";
     }
     ?>
 </section><!-- end of section-->

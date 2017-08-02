dfa001a4481303ff63f5540a9484d8734cfa7764
chisoftltd <chisoftltd@gmail.com>
2017-08-02 14:33:01 +0100

    further commit

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ include_once 'db/dbconnect.php';
                                 class="glyphicon glyphicon-user">Signed in as <?php echo $_SESSION['usr_name']; ?>
                         </p></li>
                     <li><a href="/index.php"><span class="glyphicon glyphicon-log-out">Log Out</a></li>
-                    <!--<form class="navbar-form navbar-right">
+                    <form class="navbar-form navbar-right">
                         <div class="input-group">
                             <input type="text" class="form-control" placeholder="Search">
                             <div class="input-group-btn">
@@ -71,7 +71,7 @@ include_once 'db/dbconnect.php';
                                 </button>
                             </div>
                         </div>
-                    </form>-->
+                    </form>
                 <?php } else { ?>
                     <li class="active"><a href="index.php">Home</a></>
                     <li><a href="menu/about.php">About Us</a></li>
@@ -83,7 +83,7 @@ include_once 'db/dbconnect.php';
                     <li><a href="menu/help.php">Help</a></li>
                     <li><a href="menu/login.php"><span class="glyphicon glyphicon-log-in">Login</a></li>
                     <li><a href="menu/register.php"><span class="glyphicon glyphicon-user"></span>Register</a></li>
-                    <!--<form class="navbar-form navbar-right">
+                    <form class="navbar-form navbar-right">
                         <div class="input-group">
                             <input type="text" class="form-control" placeholder="Search">
                             <div class="input-group-btn">
@@ -92,7 +92,7 @@ include_once 'db/dbconnect.php';
                                 </button>
                             </div>
                         </div>
-                    </form>-->
+                    </form>
                 <?php } ?>
             </ul>
         </div>

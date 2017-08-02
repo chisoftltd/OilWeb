4083e2b3e24524c44b1f79aabca28a35b1a9bbf0
chisoftltd <chisoftltd@gmail.com>
2017-08-02 15:26:07 +0100

    further commit

--- menu/submission.php
+++ menu/submission.php
@@ -5,3 +5,160 @@
  * Date: 30/07/2017
  * Time: 17:25
  */
+
+session_start();
+
+// include the database script
+include_once '../db/dbconnect.php';
+
+//end any active user session
+//unset($_session['user_id']);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>WebOil - Drilling</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Add JavaScript file-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script>
+    <script src="/js/weboil.js"></script>
+    <!-- Add css file-->
+    <link rel="stylesheet" href="/css/main-style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body><!-- Body area start-->
+
+<!-- add top navigational bar using bootstrap-->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navweboil">
+                <!--<span class="sr-only">Toggle navigation</span>-->
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/index.php">WebOil | Well Drilling</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navweboil">
+            <ul class="nav navbar-nav navbar-right">
+                <!-- check if same user is still same as the active session user and load appropriate menu options -->
+                <?php if (isset($_SESSION['usr_id'])) { ?>
+                    <li><a href="signinindex.php">Home</a></>
+                    <li><a href="/menu/about.php">About Us</a></li>
+                    <li><a href="/menu/courses.php">Courses</a></li>
+                    <li><a href="/menu/assessment.php">Assessment</a></li>
+                    <li class="active"><a href="/menu/submission.php">Submission</a></li>
+                    <li><a href="/menu/demo.php">Demo</a></li>
+                    <li><a href="/menu/contact.php">Contact Us</a></li>
+                    <li><a href="/menu/help.php">Help</a></li>
+                    <li><p class="navbar-text"><span
+                                class="glyphicon glyphicon-user">Signed in as <?php echo $_SESSION['usr_name']; ?>
+                        </p></li>
+                    <li><a href="/index.php"><span class="glyphicon glyphicon-log-out">Log Out</a></li>
+                    <!--<form class="navbar-form navbar-right">
+                          <div class="input-group">
+                              <input type="text" class="form-control" placeholder="Search">
+                              <div class="input-group-btn">
+                                  <button class="btn btn-default" type="submit">
+                                      <i class="glyphicon glyphicon-search"></i>
+                                  </button>
+                              </div>
+                          </div>
+                      </form>-->
+                <?php } else { ?>
+                    <li class="active"><a href="/index.php">Home</a></>
+                    <li><a href="/menu/about.php">About Us</a></li>
+                    <li class="active"><a href="/menu/courses.php">Courses</a></li>
+                    <li><a href="/menu/assessment.php">Assessment</a></li>
+                    <li><a href="/menu/submission.php">Submission</a></li>
+                    <li><a href="/menu/demo.php">Demo</a></li>
+                    <li><a href="/menu/contact.php">Contact Us</a></li>
+                    <li><a href="/menu/help.php">Help</a></li>
+                    <li><a href="/menu/login.php"><span class="glyphicon glyphicon-log-in">Login</a></li>
+                    <li><a href="/menu/register.php"><span class="glyphicon glyphicon-user"></span>Register</a></li>
+                    <!--<form class="navbar-form navbar-right">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit">
+                                    <i class="glyphicon glyphicon-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>-->
+                <?php } ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<header>
+    <?php if (isset($_SESSION['usr_id'])) { ?>
+        <?php include '../include/signinheader.php'; ?>
+    <?php } else { ?>
+        <?php include '../include/header.php'; ?><?php } ?>
+</header>
+<hr> <!-- draw a line-->
+<section><div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading"><strong>Upload Files</strong> <small>Bootstrap files upload</small></div>
+            <div class="panel-body">
+
+                <!-- Standar Form -->
+                <h4>Select files from your computer</h4>
+                <form action="" method="post" enctype="multipart/form-data" id="js-upload-form">
+                    <div class="form-inline">
+                        <div class="form-group">
+                            <input type="file" name="files[]" id="js-upload-files" multiple>
+                        </div>
+                        <button type="submit" class="btn btn-sm btn-primary" id="js-upload-submit">Upload files</button>
+                    </div>
+                </form>
+
+                <!-- Drop Zone -->
+                <h4>Or drag and drop files below</h4>
+                <div class="upload-drop-zone" id="drop-zone">
+                    Just drag and drop files here
+                </div>
+
+                <!-- Progress Bar -->
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                        <span class="sr-only">60% Complete</span>
+                    </div>
+                </div>
+
+                <!-- Upload Finished -->
+                <div class="js-upload-finished">
+                    <h3>Processed files</h3>
+                    <div class="list-group">
+                        <a href="#" class="list-group-item list-group-item-success"><span class="badge alert-success pull-right">Success</span>image-01.jpg</a>
+                        <a href="#" class="list-group-item list-group-item-success"><span class="badge alert-success pull-right">Success</span>image-02.jpg</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- /container -->
+</section><!-- end of section-->
+<form>
+    <hr> <!-- draw a line-->
+</form>
+<footer>
+    <!-- footer area-->
+    <div>
+        <?php include '../include/footer.php'; ?>
+    </div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script> <!-- jQuery library -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- Latest compiled JavaScript -->
+</body>
+</html>

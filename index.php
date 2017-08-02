85ad47454acf325de2edc8c3edc230d592641684
chisoftltd <chisoftltd@gmail.com>
2017-08-02 14:31:17 +0100

    further commit

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ include_once 'db/dbconnect.php';
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php">Home | WebOil Elearning Solution</a>
+            <a class="navbar-brand" href="index.php">Home | WebOil E-Solution</a>
         </div>
         <div class="collapse navbar-collapse" id="navweboil">
             <ul class="nav navbar-nav navbar-right">
@@ -59,7 +59,7 @@ include_once 'db/dbconnect.php';
                     <li><a href="menu/contact.php">Contact Us</a></li>
                     <li><a href="menu/help.php">Help</a></li>
                     <li><p class="navbar-text"><span
-                                    class="glyphicon glyphicon-user">Signed in as <?php echo $_SESSION['usr_name']; ?>
+                                class="glyphicon glyphicon-user">Signed in as <?php echo $_SESSION['usr_name']; ?>
                         </p></li>
                     <li><a href="/index.php"><span class="glyphicon glyphicon-log-out">Log Out</a></li>
                     <!--<form class="navbar-form navbar-right">
@@ -110,138 +110,103 @@ include_once 'db/dbconnect.php';
 </form>
 <section>
     <div class="container">
-        <h2>Topics:</h2>
-        <div class="panel panel-default">
-            <div class="row">
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <div class="topic effect">
-                        <h3>Drilling</h3>
-                        <a href=" courses/welldrilling.php"><img src="images/drilling2.jpg" alt="drilling image"
-                                                                 width="200"
-                                                                 height="200"/>
-                            <h3>Preview</h3>
-                            <p>In this topic the student is introduced to the milestone activities associated with
-                                planning for and executing a well construction programme. They will also
-                                discover in more detail the roles and responsibilities of core personnel required to
-                                plan and execute a well. Different organisational structures to deliver a well are
-                                also discussed. In addition, the student learns about generic objectives for a well
-                                and what specific information is required to allow comprehensive well planning.
-                            </p>
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-
-                    <div class="topic effect"><h3>Control</h3> <a href="courses/wellcontrol.php"><img
-                                    src="images/control.jpg" alt="drilling image"
-                                    width="200"
-                                    height="200"/>
-                            <p>This topic discusses the causes of a kick, methods of kick detection, well
-                                control procedures, and the components and function of surface and subsea well control
-                                equipment.</p>
-                    </div>
-                    </a>
-                </div>
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <div class="topic effect"><h3>Casing & Cementing</h3><a href="courses/fluids/cement.php"><img
-                                    src="images/casingcementing.jpg" alt="drilling image"
-                                    width="200"
-                                    height="200"/>
-                            <p>Here you will find information about cementing of a well to hold the formation and earth
-                                structure from collapsing.</p>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/fluids/muds.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/rigcomponents.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/wellcompletion.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/offshore/structures.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/offshore/flowlines.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/offshore/manifolds.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/oiltreatment.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/separationandcontrol.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/fluismechanicsandmachinery.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/measurement.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/storageandexport.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/treatment.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/dehydation.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/pipelinesandfassurance.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-
-                <div class="col-xs-3 col-sm-3 col-lg-3">
-                    <a href="courses/decommissioning.php">
-                        <div class="topic effect">ipsolum ushf;ahfosaif oiufdsfdgv skudhbv ofdbfds</div>
-                    </a>
-                </div>
-            </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-3 col-md-3"><a href=" courses/welldrilling.php"><img src="images/drilling2.jpg" alt="drilling image" class="img-thumbnail" width="200" height="200"></a><h3>Drilling</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><a href="courses/wellcontrol.php"><img src="images/control.jpg" alt="control image" class="img-thumbnail" width="200" height="200"></a><h3>Control</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Casing & Cementing</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
+                <p>In this topic the student is introduced to the milestone activities associated with
+                    planning for and executing a well construction programme. They will also
+                    discover in more detail the roles and responsibilities of core personnel required to
+                    plan and execute a well. Different organisational structures to deliver a well are
+                    also discussed. In addition, the student learns about generic objectives for a well
+                    and what specific information is required to allow comprehensive well planning.
+                </p></div>
         </div>
     </div>
 </section><!-- end of section-->

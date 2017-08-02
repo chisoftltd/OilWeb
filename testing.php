eb67bf2b4d10c8b2b537c0901aa423ae04697f1f
chisoftltd <chisoftltd@gmail.com>
2017-08-02 13:54:37 +0100

    further commit

--- testing.php
+++ testing.php
@@ -44,7 +44,7 @@ session_start();
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php">Home | WebOil Elearning Solution</a>
+            <a class="navbar-brand" href="index.php">Home | WebOil E-Solution</a>
         </div>
         <div class="collapse navbar-collapse" id="navweboil">
             <ul class="nav navbar-nav navbar-right">

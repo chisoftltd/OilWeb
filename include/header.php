d408879d4a2d540aced32283a5c2ca66eae85159
chisoftltd <chisoftltd@gmail.com>
2017-08-02 15:14:08 +0100

    further commit

--- include/header.php
+++ include/header.php
@@ -8,10 +8,4 @@ and open the template in the editor.
     <div id="logo">
         <a href="/index.php"><img src="/images/WebOilLogo.png" alt="Company logo"/></a>
     </div>
-    <!-- <form style="float: right; position:absolute; right: 5%; top: 10%">
-         Search:
-         <input type="search" name="googlesearch">
-         <input type="Submit">
-     </form>-->
-
 </header>

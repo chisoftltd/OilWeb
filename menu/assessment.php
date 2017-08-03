6667de398426c90cc1c3294c68e73e31ff5a04a6
chisoftltd <chisoftltd@gmail.com>
2017-08-03 23:21:15 +0100

    further commit

--- menu/assessment.php
+++ menu/assessment.php
@@ -27,8 +27,8 @@ session_start();
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <!-- Add css file-->
     <!-- <link href="css/styles.css" rel="stylesheet" type="text/css"/>-->
-    <link rel="stylesheet" href="css/main-style.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" href="/css/main-style.css">
+    <link rel="stylesheet" type="text/css" href="/css/styles.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -52,15 +52,15 @@ session_start();
                 <!-- check if same user is still same as the active session user and load appropriate menu options -->
                 <?php if (isset($_SESSION['usr_id'])) { ?>
                     <li class="active"><a href="signinindex.php">Home</a></>
-                    <li><a href="menu/about.php">About Us</a></li>
-                    <li><a href="menu/courses.php">Courses</a></li>
-                    <li><a href="menu/assessment.php">Assessment</a></li>
-                    <li><a href="menu/submission.php">Submission</a></li>
-                    <li><a href="menu/demo.php">Demo</a></li>
-                    <li><a href="menu/contact.php">Contact Us</a></li>
-                    <li><a href="menu/help.php">Help</a></li>
+                    <li><a href="/menu/about.php">About Us</a></li>
+                    <li><a href="/menu/courses.php">Courses</a></li>
+                    <li><a href="/menu/assessment.php">Assessment</a></li>
+                    <li><a href="/menu/submission.php">Submission</a></li>
+                    <li><a href="/menu/demo.php">Demo</a></li>
+                    <li><a href="/menu/contact.php">Contact Us</a></li>
+                    <li><a href="/menu/help.php">Help</a></li>
                     <li><p class="navbar-text"><span
-                                class="glyphicon glyphicon-user">Signed in as <?php echo $_SESSION['usr_name']; ?>
+                                    class="glyphicon glyphicon-user">Signed in as <?php echo $_SESSION['usr_name']; ?>
                         </p></li>
                     <li><a href="/index.php"><span class="glyphicon glyphicon-log-out">Log Out</a></li>
                     <form class="navbar-form navbar-right">
@@ -75,15 +75,15 @@ session_start();
                     </form>
                 <?php } else { ?>
                     <li class="active"><a href="index.php">Home</a></>
-                    <li><a href="menu/about.php">About Us</a></li>
-                    <li><a href="menu/courses.php">Courses</a></li>
-                    <li><a href="menu/assessment.php">Assessment</a></li>
-                    <li><a href="menu/submission.php">Submission</a></li>
-                    <li><a href="menu/demo.php">Demo</a></li>
-                    <li><a href="menu/contact.php">Contact Us</a></li>
-                    <li><a href="menu/help.php">Help</a></li>
-                    <li><a href="menu/login.php"><span class="glyphicon glyphicon-log-in">Login</a></li>
-                    <li><a href="menu/register.php"><span class="glyphicon glyphicon-user"></span>Register</a></li>
+                    <li><a href="/menu/about.php">About Us</a></li>
+                    <li><a href="/menu/courses.php">Courses</a></li>
+                    <li><a href="/menu/assessment.php">Assessment</a></li>
+                    <li><a href="/menu/submission.php">Submission</a></li>
+                    <li><a href="/menu/demo.php">Demo</a></li>
+                    <li><a href="/menu/contact.php">Contact Us</a></li>
+                    <li><a href="/menu/help.php">Help</a></li>
+                    <li><a href="/menu/login.php"><span class="glyphicon glyphicon-log-in">Login</a></li>
+                    <li><a href="/menu/register.php"><span class="glyphicon glyphicon-user"></span>Register</a></li>
                     <form class="navbar-form navbar-right">
                         <div class="input-group">
                             <input type="text" class="form-control" placeholder="Search">
@@ -102,9 +102,9 @@ session_start();
 
 <header>
     <?php if (isset($_SESSION['usr_id'])) { ?>
-        <?php include 'include/signinheader.php'; ?>
+        <?php include '../include/signinheader.php'; ?>
     <?php } else { ?>
-        <?php include 'include/header.php'; ?><?php } ?>
+        <?php include '../include/header.php'; ?><?php } ?>
 </header>
 <form>
     <hr> <!-- draw a line-->
@@ -115,7 +115,7 @@ session_start();
 
             <h1>Final Quiz for Lip building</h1>
 
-            <form action="grade.php" method="post" id="quiz">
+            <form action="/grade.php" method="post" id="quiz">
 
                 <ol>
 
@@ -274,7 +274,7 @@ session_start();
 <footer>
     <!-- footer area-->
     <div>
-        <?php include 'include/footer.php'; ?>
+        <?php include '../include/footer.php'; ?>
     </div>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script> <!-- jQuery library -->

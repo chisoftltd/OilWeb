8c1e70b32b1e19c62de9310667fa84da982fac8d
chisoftltd <chisoftltd@gmail.com>
2017-08-03 22:04:00 +0100

    further commit

--- testing.php
+++ testing.php
@@ -22,15 +22,16 @@ session_start();
     <meta charset="utf-8">
     <title>Application Description - WebOil!</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <!-- Add css file-->
     <!-- <link href="css/styles.css" rel="stylesheet" type="text/css"/>-->
     <link rel="stylesheet" href="css/main-style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body><!-- Body area start-->
 
@@ -109,106 +110,163 @@ session_start();
     <hr> <!-- draw a line-->
 </form>
 <section>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-3 col-md-3"><a href=" courses/welldrilling.php"><img src="images/drilling2.jpg" alt="drilling image" class="img-thumbnail" width="200" height="200"></a><h3>Drilling</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><a href="courses/wellcontrol.php"><img src="images/control.jpg" alt="control image" class="img-thumbnail" width="200" height="200"></a><h3>Control</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Casing & Cementing</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling" class="img-thumbnail" width="200" height="200"><h3>Preview</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-        </div>
+
+    <div id="page-wrap">
+
+        <h1>Final Quiz for Lip building</h1>
+
+        <form action="grade.php" method="post" id="quiz">
+
+            <ol>
+
+                <li>
+
+                    <h3>CSS Stands for...</h3>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A"/>
+                        <label for="question-1-answers-A">A) Computer Styled Sections </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B"/>
+                        <label for="question-1-answers-B">B) Cascading Style Sheets</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C"/>
+                        <label for="question-1-answers-C">C) Crazy Solid Shapes</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D"/>
+                        <label for="question-1-answers-D">D) None of the above</label>
+                    </div>
+
+                </li>
+
+                <li>
+
+                    <h3>Internet Explorer 6 was released in...</h3>
+
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A"/>
+                        <label for="question-2-answers-A">A) 2001</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B"/>
+                        <label for="question-2-answers-B">B) 1998</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C"/>
+                        <label for="question-2-answers-C">C) 2006</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D"/>
+                        <label for="question-2-answers-D">D) 2003</label>
+                    </div>
+
+                </li>
+
+                <li>
+
+                    <h3>SEO Stand for...</h3>
+
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A"/>
+                        <label for="question-3-answers-A">A) Secret Enterprise Organizations</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B"/>
+                        <label for="question-3-answers-B">B) Special Endowment Opportunity</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C"/>
+                        <label for="question-3-answers-C">C) Search Engine Optimization</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D"/>
+                        <label for="question-3-answers-D">D) Seals End Olives</label>
+                    </div>
+
+                </li>
+
+                <li>
+
+                    <h3>A 404 Error...</h3>
+
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A"/>
+                        <label for="question-4-answers-A">A) is an HTTP Status Code meaning Page Not Found</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B"/>
+                        <label for="question-4-answers-B">B) is a good excuse for a clever design</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C"/>
+                        <label for="question-4-answers-C">C) should be monitored for in web analytics</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D"/>
+                        <label for="question-4-answers-D">D) All of the above</label>
+                    </div>
+
+                </li>
+
+                <li>
+
+                    <h3>Your favorite website is</h3>
+
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A"/>
+                        <label for="question-5-answers-A">A) CSS-Tricks</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B"/>
+                        <label for="question-5-answers-B">B) CSS-Tricks</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C"/>
+                        <label for="question-5-answers-C">C) CSS-Tricks</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D"/>
+                        <label for="question-5-answers-D">D) CSS-Tricks</label>
+                    </div>
+
+                </li>
+
+            </ol>
+
+            <input type="submit" value="Submit Quiz"/>
+
+        </form>
+
     </div>
+
+    <script type="text/javascript">
+        var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+        document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+        var pageTracker = _gat._getTracker("UA-68528-29");
+        pageTracker._initData();
+        pageTracker._trackPageview();
+    </script>
+
 </section><!-- end of section-->
 <form>
     <hr> <!-- draw a line-->

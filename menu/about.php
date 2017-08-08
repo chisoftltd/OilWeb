1cc3e1acd1895309c5e25c5fc4a5e82326a35a93
chisoftltd <chisoftltd@gmail.com>
2017-08-08 20:13:25 +0100

    further commit

--- menu/about.php
+++ menu/about.php
@@ -58,7 +58,7 @@ if (isset($_POST['btn-login'])) {
     <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <title>RGUEthics- About Us</title>
+        <title>WebOil | About Us</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
--- menu/contact.php
+++ menu/contact.php
@@ -15,7 +15,7 @@ $error = false;
     <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <title>Coding Cage - Login & Registration System</title>
+        <title>WebOil | Contact Us</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">

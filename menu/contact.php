4b21cb01b53aefd24b7e615b4c060ef52db76629
chisoftltd <chisoftltd@gmail.com>
2017-08-08 20:09:34 +0100

    further commit

--- menu/contact.php
+++ menu/contact.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
 session_start();
-require_once 'dbconnect.php';
+require_once '../db/dbconnect.php';
 
 // it will never let you open index(login) page if session is set
 if (isset($_SESSION['user']) != "") {
@@ -21,9 +21,70 @@ $error = false;
     </head>
     <body>
     <header>
-        <div>
-            <?php include '../include/header.php'; ?>
-        </div>
+        <!-- add top navigational bar using bootstrap-->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navweboil">
+                        <!--<span class="sr-only">Toggle navigation</span>-->
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/index.php">Home | WebOil E-Solution</a>
+                </div>
+                <div class="collapse navbar-collapse" id="navweboil">
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- check if same user is still same as the active session user and load appropriate menu options -->
+                        <?php if (isset($_SESSION['usr_id'])) { ?>
+                            <li><a href="signinindex.php">Home</a></>
+                            <li><a href="/menu/about.php">About Us</a></li>
+                            <li><a href="/menu/courses.php">Courses</a></li>
+                            <li><a href="/menu/assessment.php">Assessment</a></li>
+                            <li><a href="/menu/submission.php">Submission</a></li>
+                            <li><a href="/menu/demo.php">Demo</a></li>
+                            <li class="active"><a href="/menu/contact.php">Contact Us</a></li>
+                            <li><a href="/menu/help.php">Help</a></li>
+                            <li><p class="navbar-text"><span
+                                            class="glyphicon glyphicon-user">Signed in as <?php echo $_SESSION['usr_name']; ?>
+                                </p></li>
+                            <li><a href="/index.php"><span class="glyphicon glyphicon-log-out">Log Out</a></li>
+                            <form class="navbar-form navbar-right">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-default" type="submit">
+                                            <i class="glyphicon glyphicon-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        <?php } else { ?>
+                            <li><a href="/index.php">Home</a></>
+                            <li><a href="/menu/about.php">About Us</a></li>
+                            <li><a href="/menu/courses.php">Courses</a></li>
+                            <li><a href="/menu/assessment.php">Assessment</a></li>
+                            <li><a href="/menu/submission.php">Submission</a></li>
+                            <li><a href="/menu/demo.php">Demo</a></li>
+                            <li class="active"><a href="/menu/contact.php">Contact Us</a></li>
+                            <li><a href="/menu/help.php">Help</a></li>
+                            <li><a href="/menu/login.php"><span class="glyphicon glyphicon-log-in">Login</a></li>
+                            <li><a href="/menu/register.php"><span class="glyphicon glyphicon-user"></span>Register</a></li>
+                            <form class="navbar-form navbar-right">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-default" type="submit">
+                                            <i class="glyphicon glyphicon-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
 
     <hr>

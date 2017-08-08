dc3e61f702a417f7a9c5eaf580c74b6a4e652f4f
chisoftltd <chisoftltd@gmail.com>
2017-08-08 19:46:27 +0100

    further commit

--- testing.php
+++ testing.php
@@ -705,12 +705,13 @@ if (isset($_POST['deleteform'])) {
             }
         </script>
     </div>
-    <footer>
-        <?php include 'include/footer.php'; ?>
-    </footer>
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</section>
+<footer>
+    <?php include 'include/footer.php'; ?>
+</footer>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>

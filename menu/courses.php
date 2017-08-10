db656df5f6f413301f7e21bac4acfc36e708498e
chisoftltd <chisoftltd@gmail.com>
2017-08-10 19:38:40 +0100

    further commit

--- menu/courses.php
+++ menu/courses.php
@@ -254,7 +254,7 @@ include_once '../db/dbconnect.php';
                     plan and execute a well. Different organisational structures to deliver a well are
                     also discussed. In addition, the student learns about generic objectives for a well
                     and what specific information is required to allow comprehensive well planning.
-                </p><hr> <!-- draw a line--></div>
+                </p></div>
         </div>
     </div>
 </section><!-- end of section-->

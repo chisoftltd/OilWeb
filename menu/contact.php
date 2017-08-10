7c9941aa1182f904522f97cd31d65077b6102d17
chisoftltd <chisoftltd@gmail.com>
2017-08-10 21:40:49 +0100

    further commit

--- menu/contact.php
+++ menu/contact.php
@@ -9,7 +9,47 @@ if (isset($_SESSION['user']) != "") {
     exit;
 }
 
-$error = false;
+
+//if statement to process a POST data from a form when a submit buuton is clicked
+if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $human = intval($_POST['human']);
+    $from = 'Demo Contact Form';
+    $to = 'example@bootstrapbay.com';
+    $subject = 'Message from Contact Demo ';
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+    // Check if name has been entered
+    if (!$_POST['name']) {
+        $errName = 'Please enter your name';
+    }
+
+    // Check if email has been entered and is valid
+    if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errEmail = 'Please enter a valid email address';
+    }
+
+    //Check if message has been entered
+    if (!$_POST['message']) {
+        $errMessage = 'Please enter your message';
+    }
+    //Check if simple anti-bot test is correct
+    if ($human !== 5) {
+        $errHuman = 'Your anti-spam is incorrect';
+    }
+
+    // If there are no errors, send the email
+    if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+        if (mail($to, $subject, $body, $from)) {
+            $result = '<div class="alert alert-success">Thank You! I will be in touch</div>';
+        } else {
+            $result = '<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+        }
+    }
+}
 ?>
     <!DOCTYPE html>
     <html>
@@ -98,18 +138,103 @@ $error = false;
 
     <hr>
     <section>
-        <div class="container" style="margin-top: 50px; background-color:#b0e0e6; border:3px solid #006400;">
-            <div class="panel-heading">Tell us where we are doing badly - WebOil</div>
-            <div class="panel-body">
-                <form action="sendemail.php" method="post" style="margin: 0 auto; width:250px;">
-                    <label for="subject">Subject of email:</label><br>
-                    <input type="text" name="subject" id="subject"/><br>
-                    <label for="body">Body of email:</label><br>
-                    <textarea name="body" id="body" rows="10" cols="35"></textarea><br>
-                    <input type="submit" name=submit value="Submit"/>
-                </form>
-            </div>
-        </div>
+        <div class="container"> <!--div with address and map information-->
+            <form>
+                <div class="row">
+                    <div id="map-outer" class="col-md-12">
+                        <div id="address" class="col-md-4">
+                            <h2>Our Location</h2>
+                            <address>
+                                <strong>RGUEthics | Research Ethics Solution</strong><br>
+                                Robert Gordon University<br>
+                                Garthdee House<br>
+                                Garthdee Road<br>
+                                Aberdeen<br>
+                                AB10 7QB<br>
+                                Scotland<br>
+                                United Kingdom<br>
+                                <abbr>P:</abbr> +44 1224 262000
+                            </address>
+                        </div>
+                        <div id="map-container" class="col-md-8"></div>
+                        <script> //function to get map location
+
+                            function init_map() {
+                                var var_location = new google.maps.LatLng(57.1184, -2.1410);
+
+                                var var_mapoptions = {
+                                    center: var_location,
+                                    zoom: 14
+                                };
+
+                                var var_marker = new google.maps.Marker({
+                                    position: var_location,
+                                    map: var_map,
+                                    title: "Venice"
+                                });
+
+                                var var_map = new google.maps.Map(document.getElementById("map-container"),
+                                    var_mapoptions);
+
+                                var_marker.setMap(var_map);
+
+                            }
+
+                            google.maps.event.addDomListener(window, 'load', init_map);
+
+                        </script>
+                    </div>
+                </div>
+            </form>
+        </div><!-- end of div container -->
+        <form>
+            <hr>
+        </form>
+        <div class="container"><!-- div for accepting messages-->
+            <form class="form-horizontal" role="form" method="post" action="index.php">
+                <div class="form-group">
+                    <label for="name" class="col-sm-2 control-label">Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name"
+                               value="<?php echo htmlspecialchars($_POST['name']); ?>">
+                        <?php echo "<p class='text-danger'>$errName</p>"; ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="email" class="col-sm-2 control-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com"
+                               value="<?php echo htmlspecialchars($_POST['email']); ?>">
+                        <?php echo "<p class='text-danger'>$errEmail</p>"; ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="message" class="col-sm-2 control-label">Message</label>
+                    <div class="col-sm-10">
+                    <textarea class="form-control" rows="4"
+                              name="message"><?php echo htmlspecialchars($_POST['message']); ?></textarea>
+                        <?php echo "<p class='text-danger'>$errMessage</p>"; ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+                        <?php echo "<p class='text-danger'>$errHuman</p>"; ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-2">
+                        <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-2">
+                        <?php echo $result; ?>
+                    </div>
+                </div>
+            </form>
+        </div><!-- end of content div-->
     </section>
     <hr>
     <footer>

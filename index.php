ee5e84fa9ae0d4ba344f3160e1471e22828e5d84
chisoftltd <chisoftltd@gmail.com>
2017-08-19 16:44:48 +0100

    further commit

--- index.php
+++ index.php
@@ -105,6 +105,8 @@ include_once 'db/dbconnect.php';
 <section>
     <div class="container">
         <div class="row">
+            <h2>Welcome to WebOil</h2>
+            <p>An online platform to help you on your way to excieting and sustaianable career in <strong>Oil and Gas</strong>strong> Industry</p>
             <div class="col-xs-12 col-sm-3 col-md-3"><a href=" courses/welldrilling.php"><img src="images/drilling.jpg"
                                                                                               alt="drilling image"
                                                                                               class="img-thumbnail"
@@ -123,22 +125,29 @@ include_once 'db/dbconnect.php';
                                                                                             width="200"
                                                                                             height="200"></a>
                 <h3>Control</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
-                </p></div>
-            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling"
+                <p>This topic discusses the causes of a kick, methods of kick detection, well control procedures, and
+                    the components and function of surface and subsea well control equipment.
+
+                </p>
+                <p>this topic covers Operational Pressures and will give students a
+                    good understanding of the pressure relationships in the wellbore and rock
+                    formation.</p>
+                <p>
+                    Formation Pressure is discussed next and describes what formation pressure is,
+                    its relevance, its relationship with other pressures and its evaluation.</p>
+                <p>
+                    Fracture Pressure is covered last, defining what fracture pressure is, its relevance
+                    and evaluation.</p></div>
+            <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/casing&cementing.jpg" alt="Drilling"
                                                           class="img-thumbnail" width="200" height="200">
                 <h3>Casing & Cementing</h3>
-                <p>In this topic the student is introduced to the milestone activities associated with
-                    planning for and executing a well construction programme. They will also
-                    discover in more detail the roles and responsibilities of core personnel required to
-                    plan and execute a well. Different organisational structures to deliver a well are
-                    also discussed. In addition, the student learns about generic objectives for a well
-                    and what specific information is required to allow comprehensive well planning.
+                <p>This topic is designed for use by students to gain an understanding of the principles behind the use
+                    of casing within the wellbore and the factors involved in casing string design.</p>
+                <p>Another topic covered are the functions of oilwell cement, the API classification and
+                    properties of dry cement and neat slurry and the effect of additives on these
+                    properties. Primary and stage cementing procedures are described along with the
+                    equipment used e.g, float collars, stage collars, surface equipment, centralisers
+                    and scratchers. Inner string and liner cementing are covered.
                 </p></div>
             <div class="col-xs-12 col-sm-3 col-md-3"><img src="images/drilling2.jpg" alt="Drilling"
                                                           class="img-thumbnail" width="200" height="200">

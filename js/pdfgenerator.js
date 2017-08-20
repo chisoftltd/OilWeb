a9676908beaca01aca92158114e1321f940f16d4
chisoftltd <chisoftltd@gmail.com>
2017-08-20 17:50:55 +0100

    further commit

--- js/pdfgenerator.js
+++ js/pdfgenerator.js
@@ -2,7 +2,6 @@
  * Created by 1609963 on 20/08/2017.
  */
 var doc = new jsPDF();
-doc.setFont("courier");
 var specialElementHandlers = {
     '#editor': function (element, renderer) {
         return true;

77d193f881b5b8816191c8c995cf0b37265ad1a7
chisoftltd <chisoftltd@gmail.com>
2017-08-20 02:05:58 +0100

    further commit

--- index.php
+++ index.php
@@ -106,7 +106,7 @@ include_once 'db/dbconnect.php';
     <div class="container">
         <div class="row">
             <h1 style="text-align: center">Welcome to WebOil</h1>
-            <h2 style="text-align: center>An online platform to help you on your way to exciting and sustainable career in <strong>Oil and
+            <h2 style="text-align: center">An online platform to help you on your way to exciting and sustainable career in <strong>Oil and
                     Gas</strong> Industry.</h2>
             <form>
                 <hr>

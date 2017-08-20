3c4035c11cd0b231a888d2bead4ddd503577a314
chisoftltd <chisoftltd@gmail.com>
2017-08-20 17:54:16 +0100

    further commit

--- courses/welldrilling.php
+++ courses/welldrilling.php
@@ -27,8 +27,24 @@ include_once '../db/dbconnect.php';
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script>
     <script src="/js/weboil.js"></script>
-    <script src="/js/pdfgenerator.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.debug.js"></script>
+    <script>
+        var doc = new jsPDF();
+        doc.setFont("courier");
+        var specialElementHandlers = {
+            '#editor': function (element, renderer) {
+                return true;
+            }
+        };
+
+        $('#cmd').click(function () {
+            doc.fromHTML($('#seite2').html(), 15, 15, {
+                'width': 3170,
+                'elementHandlers': specialElementHandlers
+            });
+            doc.save('Well-Planning-Objectives.pdf');
+        });
+    </script>
     <!-- Add css file-->
     <link rel="stylesheet" href="/css/main-style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">

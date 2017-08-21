71cd33dda953439c5eef0d993eff266c25848e21
chisoftltd <chisoftltd@gmail.com>
2017-08-21 08:44:17 +0100

    further commit

--- index.php
+++ index.php
@@ -109,6 +109,30 @@ include_once 'db/dbconnect.php';
             <h2 style="text-align: center">An online platform to help you on your way to exciting and sustainable career
                 in <strong>Oil and
                     Gas</strong> Industry.</h2>
+
+            <div class="w3-content w3-section" style="max-width:500px">
+                <img class="mySlides" src="images/drilling.jpg" style="width:100%">
+                <img class="mySlides" src="images/control.jpg" style="width:100%">
+                <img class="mySlides" src="images/casingcementing.jpg" style="width:100%">
+            </div>
+
+            <script>
+                var myIndex = 0;
+                carousel();
+
+                function carousel() {
+                    var i;
+                    var x = document.getElementsByClassName("mySlides");
+                    for (i = 0; i < x.length; i++) {
+                        x[i].style.display = "none";
+                    }
+                    myIndex++;
+                    if (myIndex > x.length) {myIndex = 1}
+                    x[myIndex-1].style.display = "block";
+                    setTimeout(carousel, 2000); // Change image every 2 seconds
+                }
+            </script>
+            
             <form>
                 <hr>
             </form>

107cde5195f69fe8e88df2856901010d704ebef1
chisoftltd <chisoftltd@gmail.com>
2017-08-21 09:14:17 +0100

    further commit

--- index.php
+++ index.php
@@ -115,28 +115,46 @@ include_once 'db/dbconnect.php';
                 in <strong>Oil and
                     Gas</strong> Industry.</h2>
             <hr>
-            <div class="w3-content w3-section" style="max-width:1000px; max-height: 500px">
+
+            <div style="max-width:800px">
                 <img class="mySlides" src="images/drilling.jpg" style="width:100%">
                 <img class="mySlides" src="images/control.jpg" style="width:100%">
                 <img class="mySlides" src="images/casingcementing.jpg" style="width:100%">
+                <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+                    <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
+                    <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
+                    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+                    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+                    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+                </div>
             </div>
 
             <script>
-                var myIndex = 0;
-                carousel();
+                var slideIndex = 1;
+                showDivs(slideIndex);
 
-                function carousel() {
+                function plusDivs(n) {
+                    showDivs(slideIndex += n);
+                }
+
+                function currentDiv(n) {
+                    showDivs(slideIndex = n);
+                }
+
+                function showDivs(n) {
                     var i;
                     var x = document.getElementsByClassName("mySlides");
+                    var dots = document.getElementsByClassName("demo");
+                    if (n > x.length) {slideIndex = 1}
+                    if (n < 1) {slideIndex = x.length}
                     for (i = 0; i < x.length; i++) {
                         x[i].style.display = "none";
                     }
-                    myIndex++;
-                    if (myIndex > x.length) {
-                        myIndex = 1
+                    for (i = 0; i < dots.length; i++) {
+                        dots[i].className = dots[i].className.replace(" w3-white", "");
                     }
-                    x[myIndex - 1].style.display = "block";
-                    setTimeout(carousel, 2000); // Change image every 2 seconds
+                    x[slideIndex-1].style.display = "block";
+                    dots[slideIndex-1].className += " w3-white";
                 }
             </script>
 

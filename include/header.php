790ca1ab49d0fa739e29bd8387952dd0b3126b01
chisoftltd <chisoftltd@gmail.com>
2017-08-21 21:32:50 +0100

    further commit

--- include/header.php
+++ include/header.php
@@ -7,16 +7,19 @@ and open the template in the editor.
 <header>
     <div id="logo">
         <a href="/index.php"><img src="/images/WebOilLogo.png" alt="Company logo"/></a>
-        <div style="float: left"><p>Translate this page:</p>
+        
+    </div>
+    <div style="float: left"><p>Translate this page:</p>
 
-            <div id="google_translate_element"></div>
+        <div id="google_translate_element"></div>
 
-            <script type="text/javascript">
-                function googleTranslateElementInit() {
-                    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-                }
-            </script>
+        <script type="text/javascript">
+            function googleTranslateElementInit() {
+                new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+            }
+        </script>
 
-            <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script></div>
+        <script type="text/javascript"
+                src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     </div>
 </header>

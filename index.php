62d67e96a8179b90baa3f6ac4f18a2bb4e05b4b6
chisoftltd <chisoftltd@gmail.com>
2017-08-21 09:40:53 +0100

    further commit

--- index.php
+++ index.php
@@ -35,6 +35,17 @@ include_once 'db/dbconnect.php';
     <style>
         .mySlides {
             display: none;
+
+        .w3-left, .w3-right, .w3-badge {
+            cursor: pointer
+        }
+
+        .w3-badge {
+            height: 13px;
+            width: 13px;
+            padding: 0
+        }
+
         }
     </style>
 </head>
@@ -121,7 +132,8 @@ include_once 'db/dbconnect.php';
                 <img class="mySlides" src="images/drilling.jpg" style="width:100%; height: 50%">
                 <img class="mySlides" src="images/control.jpg" style="width:100%; height: 50%">
                 <img class="mySlides" src="images/casingcementing.jpg" style="width:100%; height: 50%">
-                <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+                <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle"
+                     style="width:100%">
                     <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
                     <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
                     <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
@@ -146,16 +158,20 @@ include_once 'db/dbconnect.php';
                     var i;
                     var x = document.getElementsByClassName("mySlides");
                     var dots = document.getElementsByClassName("demo");
-                    if (n > x.length) {slideIndex = 1}
-                    if (n < 1) {slideIndex = x.length}
+                    if (n > x.length) {
+                        slideIndex = 1
+                    }
+                    if (n < 1) {
+                        slideIndex = x.length
+                    }
                     for (i = 0; i < x.length; i++) {
                         x[i].style.display = "none";
                     }
                     for (i = 0; i < dots.length; i++) {
                         dots[i].className = dots[i].className.replace(" w3-white", "");
                     }
-                    x[slideIndex-1].style.display = "block";
-                    dots[slideIndex-1].className += " w3-white";
+                    x[slideIndex - 1].style.display = "block";
+                    dots[slideIndex - 1].className += " w3-white";
                 }
             </script>
 

34d531aeefc32ac2aa491001f0da56ba2f0246f5
chisoftltd <chisoftltd@gmail.com>
2017-08-22 17:42:42 +0100

    further commit

--- grade.php
+++ grade.php
@@ -246,6 +246,15 @@ session_start();
         </div>
     </div>
 
+    <script type="text/javascript">
+        var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+        document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+        var pageTracker = _gat._getTracker("UA-68528-29");
+        pageTracker._initData();
+        pageTracker._trackPageview();
+    </script>
 
 </section><!-- end of section-->
 <form>

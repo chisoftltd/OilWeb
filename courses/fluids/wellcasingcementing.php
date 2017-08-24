d67361e45357c6a16465b2da7f1b6d1bfe4f6a16
chisoftltd <chisoftltd@gmail.com>
2017-08-24 10:38:50 +0100

    Further improvement

--- courses/fluids/wellcasingcementing.php
+++ courses/fluids/wellcasingcementing.php
@@ -50,8 +50,6 @@
                         <li><a href="/menu/about.php">About Us</a></li>
                         <li class="active"><a href="/menu/courses.php">Courses</a></li>
                         <li><a href="/menu/assessment.php">Test Yourself</a></li>
-                        <li><a href="/menu/submission.php">Submission</a></li>
-
                         <li><a href="/menu/contact.php">Contact Us</a></li>
                         <li><a href="/menu/help.php">Help</a></li>
                         <li><p class="navbar-text"><span
@@ -73,8 +71,6 @@
                         <li><a href="/menu/about.php">About Us</a></li>
                         <li class="active"><a href="/menu/courses.php">Courses</a></li>
                         <li><a href="/menu/assessment.php">Test Yourself</a></li>
-                        <li><a href="/menu/submission.php">Submission</a></li>
-
                         <li><a href="/menu/contact.php">Contact Us</a></li>
                         <li><a href="/menu/help.php">Help</a></li>
                         <li><a href="/menu/login.php"><span class="glyphicon glyphicon-log-in">Login</a></li>

3cc59ceddc61084ce1e45111d55d3ff1a4dc4b37
chisoftltd <chisoftltd@gmail.com>
2017-08-24 18:12:41 +0100

    Further improvement

--- gradecontrol.php
+++ gradecontrol.php
@@ -335,7 +335,7 @@ session_start();
             <footer class="panel-footer clearfix ">
                 <address class="pull-right">&copy; RGU
                 </address>
-                <button id="printanswer">Generate PDF</button>
+                <button id="page-wrap">Generate PDF</button>
             </footer>
         </div>
     </div>

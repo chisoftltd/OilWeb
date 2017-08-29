a87e0b93366a49254ad92205cd914c6382180c5e
chisoftltd <chisoftltd@gmail.com>
2017-08-29 16:20:51 +0100

    Further improvement

--- menu/courses.php
+++ menu/courses.php
@@ -106,21 +106,21 @@ include_once '../db/dbconnect.php';
 <section>
 
     <div class="container" style="background-color: #b0e0e6">
-        <div>
+        
         <span class="list-style-buttons">
     <a href="#" id="gridview" class="switcher"><img src="/images/grid-view.png" alt="Grid" width="20"
-                                                    height="22" align="right"></a>
+                                                    height="22" align="right" hspace="20"></a>
     <a href="#" id="listview" class="switcher active"><img src="/images/list-view-active.png" alt="List" width="20"
                                                            height="22" align="right"></a>
 </span>
             <form>
                 <hr>
             </form>
-        </div>
+
         <div class="row">
             <div class="col-xs-12 col-sm-11 col-md-11"><a href="/courses/welldrilling.php"><img
                             src="/images/drilling.jpg" alt="drilling image" class="img-thumbnail gap-right" width="100"
-                            height="120" align="left" hspace="20"></a>
+                            height="120" align="left" hspace="20" hspace="20"></a>
                 <h3><a href="/courses/welldrilling.php">Drilling</a></h3>
                 <p>In this course you will learn more about the student is introduced to the milestone activities
                     associated with

0b84c7b91da2bd2aee4d0ea3f5c6172d5c571c7f
chisoftltd <chisoftltd@gmail.com>
2017-09-01 16:08:47 +0100

    further improvement

--- menu/login.php
+++ menu/login.php
@@ -23,6 +23,8 @@ if (isset($_POST['login'])) {
     if ($row = mysqli_fetch_array($result)) {
         $_SESSION['usr_id'] = $row['id'];
         $_SESSION['usr_name'] = $row['name'];
+        $login = "Login Successful";
+        echo "<script type='text/javascript'>alert('$login');</script>";
         header("Location: ../index.php");
     } else {
         $errormsg = "Incorrect Email or Password!!!";

afa40561a92bf7d2d0cf233599bf6ccd7223e1a8
chisoftltd <chisoftltd@gmail.com>
2017-09-01 16:12:06 +0100

    further improvement

--- include/signinheader.php
+++ include/signinheader.php
@@ -6,17 +6,4 @@ Header information when user is signed on or logged in, with company logo
     <div id="logo">
         <a href="/index.php" style="top: 10px"><img src="/images/OilWebLogo.png" alt="Company logo"/></a>
     </div>
-    <nav>
-        <ul class="header-links">
-            <li><a href="/index.php">Home</a></li>
-            <li><a href="/menu/about.php">About Us</a></li>
-            <li><a href="/menu/courses.php">Courses</a></li>
-            <li><a href="/menu/assessment.php">Test Yourself</a></li>
-            <li><a href="/menu/submission.php">Submission</a></li>
-            <li><a href="/menu/contact.php">Contact Us</a></li>
-            <li><a href="/menu/help.php">Help</a></li>
-            <li><a href="/menu/login.php">LogOut</a></li>
-            <li><a href="/menu/register.php">Register</a></li>
-        </ul>
-    </nav>
 </header>

8a617cfd3bea9c486200c01d0eef1228c7397692
chisoftltd <chisoftltd@gmail.com>
2017-09-04 11:40:53 +0100

    further commit

--- include/footer.php
+++ include/footer.php
@@ -10,12 +10,12 @@
     <ul class="footer-links">
         <li><a href="/index.php">Home</a></li>
         <!-- Add font awesome icons -->
-        <a href="/include/socialmedia.php" class="fa fa-facebook"></a>
-        <a href="/include/socialmedia.php" class="fa fa-twitter"></a>
-        <a href="/include/socialmedia.php" class="fa fa-google"></a>
-        <a href="/include/socialmedia.php" class="fa fa-linkedin"></a>
-        <a href="/include/socialmedia.php" class="fa fa-youtube"></a>
-        <a href="/include/socialmedia.php" class="fa fa-instagram"></a>
+        <li><a href="/include/socialmedia.php" class="fa fa-facebook"></a></li>
+        <li><a href="/include/socialmedia.php" class="fa fa-twitter"></a></li>
+        <li><a href="/include/socialmedia.php" class="fa fa-google"></a></li>
+        <li><a href="/include/socialmedia.php" class="fa fa-linkedin"></a></li>
+        <li><a href="/include/socialmedia.php" class="fa fa-youtube"></a></li>
+        <li><a href="/include/socialmedia.php" class="fa fa-instagram"></a></li>
         <li><a href="/menu/contact.php">Contact</a></li>
     </ul>
     <p style="text-align: center;">

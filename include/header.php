e2a0d547b45b27657afd706195e504cd24ca57e1
chisoftltd <chisoftltd@gmail.com>
2017-09-04 11:33:39 +0100

    further commit

--- include/header.php
+++ include/header.php
@@ -5,38 +5,38 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 
-    <div>
-        <div id="logo">
-            <a href="/index.php"><img src="/images/OilWebLogo.png" alt="Company logo"/></a>
-        </div>
-        <!--<div id="search">
-            <form class="navbar-form navbar-right">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                    <div class="input-group-btn">
-                        <button class="btn btn-default" type="submit">
-                            <i class="glyphicon glyphicon-search"></i>
-                        </button>
-                    </div>
+<div>
+    <div id="logo">
+        <a href="/index.php"><img src="/images/OilWebLogo.png" alt="Company logo"/></a>
+    </div>
+    <!--<div id="search">
+        <form class="navbar-form navbar-right">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit">
+                        <i class="glyphicon glyphicon-search"></i>
+                    </button>
                 </div>
-            </form>
-        </div>-->
+            </div>
+        </form>
+    </div>-->
 
-    </div>
+</div>
 
-    <div style="float: right; margin-top: inherit"><p>For other languages:</p>
+<div style="float: right; margin-top: inherit"><p>For other languages:</p>
 
-        <div id="otherlanguages"></div>
+    <div id="otherlanguages"></div>
 
-        <script type="text/javascript">
-            function otherlanguages() {
-                new google.translate.TranslateElement({
-                    pageLanguage: 'en',
-                    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-                }, 'otherlanguages');
-            }
-        </script>
+    <script type="text/javascript">
+        function otherlanguages() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en',
+                layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+            }, 'otherlanguages');
+        }
+    </script>
 
-        <script type="text/javascript"
-                src="//translate.google.com/translate_a/element.js?cb=otherlanguages"></script>
-    </div>
+    <script type="text/javascript"
+            src="//translate.google.com/translate_a/element.js?cb=otherlanguages"></script>
+</div>
--- include/signinheader.php
+++ include/signinheader.php
@@ -2,37 +2,36 @@
 <!--
 Header information when user is signed on or logged in, with company logo
 -->
-<header>
-    <div id="logo">
-        <a href="/index.php" style="top: 10px"><img src="/images/OilWebLogo.png" alt="Company logo"/></a>
-    </div>
-    <!--<nav>
-        <ul class="header-links">
-            <li><a href="/index.php">Home</a></li>
-            <li><a href="/menu/about.php">About Us</a></li>
-            <li><a href="/menu/courses.php">Courses</a></li>
-            <li><a href="/menu/assessment.php">Test Yourself</a></li>
-            <li><a href="/menu/submission.php">Submission</a></li>
-            <li><a href="/menu/contact.php">Contact Us</a></li>
-            <li><a href="/menu/help.php">Help</a></li>
-            <li><a href="/menu/login.php">LogOut</a></li>
-            <li><a href="/menu/register.php">Register</a></li>
-        </ul>
-    </nav>-->
-    <div style="float: right; margin-top: inherit"><p>For other languages:</p>
 
-        <div id="other languages"></div>
+<div id="logo">
+    <a href="/index.php" style="top: 10px"><img src="/images/OilWebLogo.png" alt="Company logo"/></a>
+</div>
+<!--<nav>
+    <ul class="header-links">
+        <li><a href="/index.php">Home</a></li>
+        <li><a href="/menu/about.php">About Us</a></li>
+        <li><a href="/menu/courses.php">Courses</a></li>
+        <li><a href="/menu/assessment.php">Test Yourself</a></li>
+        <li><a href="/menu/submission.php">Submission</a></li>
+        <li><a href="/menu/contact.php">Contact Us</a></li>
+        <li><a href="/menu/help.php">Help</a></li>
+        <li><a href="/menu/login.php">LogOut</a></li>
+        <li><a href="/menu/register.php">Register</a></li>
+    </ul>
+</nav>-->
+<div style="float: right; margin-top: inherit"><p>For other languages:</p>
 
-        <script type="text/javascript">
-            function otherlanguages() {
-                new google.translate.TranslateElement({
-                    pageLanguage: 'en',
-                    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-                }, 'other languages');
-            }
-        </script>
+    <div id="other languages"></div>
 
-        <script type="text/javascript"
-                src="//translate.google.com/translate_a/element.js?cb=otherlanguages"></script>
-    </div>
-</header>
+    <script type="text/javascript">
+        function otherlanguages() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en',
+                layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+            }, 'other languages');
+        }
+    </script>
+
+    <script type="text/javascript"
+            src="//translate.google.com/translate_a/element.js?cb=otherlanguages"></script>
+</div>

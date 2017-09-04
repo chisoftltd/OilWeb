2190f0189f7077e9ed17d56f561e08d7edd8d7c2
chisoftltd <chisoftltd@gmail.com>
2017-09-04 11:32:54 +0100

    further commit

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<header>
+
     <div>
         <div id="logo">
             <a href="/index.php"><img src="/images/OilWebLogo.png" alt="Company logo"/></a>
@@ -26,18 +26,17 @@ and open the template in the editor.
 
     <div style="float: right; margin-top: inherit"><p>For other languages:</p>
 
-        <div id="other languages"></div>
+        <div id="otherlanguages"></div>
 
         <script type="text/javascript">
             function otherlanguages() {
                 new google.translate.TranslateElement({
                     pageLanguage: 'en',
                     layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-                }, 'other languages');
+                }, 'otherlanguages');
             }
         </script>
 
         <script type="text/javascript"
                 src="//translate.google.com/translate_a/element.js?cb=otherlanguages"></script>
     </div>
-</header>
--- index.php
+++ index.php
@@ -19,8 +19,8 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
     <title>OilWeb | Elearning Application</title>
+    <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">

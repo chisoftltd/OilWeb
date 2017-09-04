c8da9fcd5996a95005438b893a4caa67d83e2b27
chisoftltd <chisoftltd@gmail.com>
2017-09-04 11:38:31 +0100

    further commit

--- include/footer.php
+++ include/footer.php
@@ -1,25 +1,24 @@
 <!-- footer file - For lack of time link here are directed to certain RGU pages-->
-<footer>
-    <nav>
-        <form>
-            <hr/>
-        </form>
-        <p style="text-align: center;">
-            <strong>Follow us</strong>
-        </p>
-        <ul class="footer-links">
-            <li><a href="/index.php">Home</a></li>
-            <!-- Add font awesome icons -->
-            <a href="/include/socialmedia.php" class="fa fa-facebook"></a>
-            <a href="/include/socialmedia.php" class="fa fa-twitter"></a>
-            <a href="/include/socialmedia.php" class="fa fa-google"></a>
-            <a href="/include/socialmedia.php" class="fa fa-linkedin"></a>
-            <a href="/include/socialmedia.php" class="fa fa-youtube"></a>
-            <a href="/include/socialmedia.php" class="fa fa-instagram"></a>
-            <li><a href="/menu/contact.php">Contact</a></li>
-        </ul>
-        <p style="text-align: center;">
-            &copy; 2016-<?php echo date("Y"); ?>
-        </p>
-    </nav>
-</footer>
+
+<nav>
+    <form>
+        <hr/>
+    </form>
+    <p style="text-align: center;">
+        <strong>Follow us</strong>
+    </p>
+    <ul class="footer-links">
+        <li><a href="/index.php">Home</a></li>
+        <!-- Add font awesome icons -->
+        <a href="/include/socialmedia.php" class="fa fa-facebook"></a>
+        <a href="/include/socialmedia.php" class="fa fa-twitter"></a>
+        <a href="/include/socialmedia.php" class="fa fa-google"></a>
+        <a href="/include/socialmedia.php" class="fa fa-linkedin"></a>
+        <a href="/include/socialmedia.php" class="fa fa-youtube"></a>
+        <a href="/include/socialmedia.php" class="fa fa-instagram"></a>
+        <li><a href="/menu/contact.php">Contact</a></li>
+    </ul>
+    <p style="text-align: center;">
+        &copy; 2016-<?php echo date("Y"); ?>
+    </p>
+</nav>
